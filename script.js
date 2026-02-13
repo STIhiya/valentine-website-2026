78010ff574ff754c0b010f5951e069805c2add06
STIhiya <81674536+STIhiya@users.noreply.github.com>
2026-02-13 17:09:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -146,16 +146,16 @@ loveMeter.addEventListener('input', () => {
     
     if (value > 100) {
         extraLove.classList.remove('hidden');
-        const overflowPercentage = (value - 100) / 9900;
+        const overflowPercentage = (value - 0) / 99;
         const extraWidth = overflowPercentage * window.innerWidth * 0.8;
         loveMeter.style.width = `calc(100% + ${extraWidth}px)`;
         loveMeter.style.transition = 'width 0.3s';
         
         // Show different messages based on the value
-        if (value >= 5000) {
+        if (value >= 100) {
             extraLove.classList.add('super-love');
             extraLove.textContent = config.loveMessages.extreme;
-        } else if (value > 1000) {
+        } else if (value > 0) {
             extraLove.classList.remove('super-love');
             extraLove.textContent = config.loveMessages.high;
         } else {
@@ -164,7 +164,7 @@ loveMeter.addEventListener('input', () => {
         }
     } else {
         extraLove.classList.add('hidden');
-        extraLove.classList.remove('111111111');
+        extraLove.classList.remove('super-love');
         loveMeter.style.width = '100%';
     }
 });
